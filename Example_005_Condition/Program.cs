8d5e367807e6f6270e67ac26b3c1aad429f3ae94
Aliya <autyabaeva@mail.ru>
2022-11-23 17:38:23 +0400

    Condition

---
+++ Example_005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input your name:");
+string username = Console.ReadLine();
+
+if(username == "Masha")
+{
+    Console.WriteLine("Hi, dear");
+}
+else
+{
+    Console.Write("Hello,");
+    Console.Write(username);
+}
